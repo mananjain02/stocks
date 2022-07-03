d5497b5a4988d6f6754d793deba9acdea627e897
Manan Jain <mananjain@Manans-MacBook-Air.local>
2022-07-03 10:48:36 +0530

    fixed some bugs

--- remove_stock/views.py
+++ remove_stock/views.py
@@ -8,9 +8,12 @@ from django.urls import reverse
 class RemoveStockView(View):
     def get(self, request):
         all_stocks = StockList.objects.filter(user=request.user)
+        error_message = ""
+        if len(all_stocks) is 0:
+            error_message = "No stocks added"
         context = {
             "all_stocks": all_stocks,
-            "error_message": "No stocks added",
+            "error_message": error_message,
         }
         return render(request, 'remove_stock/remove-stock.html', context)
 
@@ -20,7 +23,11 @@ class RemoveStockView(View):
         obj1 = obj.filter(user=request.user)
         obj1.delete()
         all_stocks = StockList.objects.filter(user=request.user)
+        error_message = ""
+        if len(all_stocks) is 0:
+            error_message = "No stocks added"
         context = {
-            "all_stocks": all_stocks
+            "all_stocks": all_stocks,
+            "error_message": error_message,
         }
         return render(request, 'remove_stock/remove-stock.html', context)
