f8e69de778b5b15ad921333dde76035e9ceecae2
Manan Jain <mananjain@Manans-MacBook-Air.local>
2022-07-03 10:41:48 +0530

    added the filter when deleting, stock now disappear after adding, fixed many urls

--- add_stock/views.py
+++ add_stock/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
 from stocks_list.models import StockList
@@ -9,23 +9,33 @@ from add_stock.models import AllStocks
 class AddStockView(View):
     def get(self, request):
         all_stocks = AllStocks.objects.all()
+        already_added_stocks = StockList.objects.filter(user=request.user)
+        already_added_stocks_name = []
+        stocks_to_show_to_add = []
+        for stock in already_added_stocks:
+            already_added_stocks_name.append(stock.name)
+        
+        for stock in all_stocks:
+            if stock.name not in already_added_stocks_name:
+                stocks_to_show_to_add.append(stock)
+
         context = {
-            "all_stocks": all_stocks
+            "all_stocks": stocks_to_show_to_add,
         }
         return render(request, 'add_stock/add-stock.html', context)
 
     def post(self, request):
-        all_stocks = StockList.objects.all()
-        for stock in all_stocks:
+        already_added_stocks = StockList.objects.filter(user=request.user)
+        for stock in already_added_stocks:
             if stock.symbol == request.POST['symbol']:
                 return render(request, 'add_stock/add-stock.html', {
                     "error_message": f"{ stock.name } is already added",
-                    "all_stocks": AllStocks.objects.all()
+                    "all_stocks": AllStocks.objects.all(),
                 })
         stock_to_add = StockList(
             name = request.POST['name'],
             symbol = request.POST['symbol'],
-            user = request.user
+            user = request.user,
         )
         stock_to_add.save()
         return HttpResponseRedirect(reverse('addstock'))
--- remove_stock/views.py
+++ remove_stock/views.py
@@ -1,21 +1,25 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from stocks_list.models import StockList
+from django.urls import reverse
 
 # Create your views here.
 class RemoveStockView(View):
     def get(self, request):
         all_stocks = StockList.objects.filter(user=request.user)
         context = {
-            "all_stocks": all_stocks
+            "all_stocks": all_stocks,
+            "error_message": "No stocks added",
         }
         return render(request, 'remove_stock/remove-stock.html', context)
 
     def post(self, request):
-        # Need to add filter to filter the user 
+        # Need to use the method to run multiple filters at one time
         obj = StockList.objects.filter(name=request.POST['name'])
-        obj.delete()
-        all_stocks = StockList.objects.all()
+        obj1 = obj.filter(user=request.user)
+        obj1.delete()
+        all_stocks = StockList.objects.filter(user=request.user)
         context = {
             "all_stocks": all_stocks
         }
